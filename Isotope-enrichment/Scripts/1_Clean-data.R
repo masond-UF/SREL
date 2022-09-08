6164b12ca1642c047333ad0a6b7d4911864d8207
David Mason <masond@ufl.edu>
2022-09-07 20:53:10 -0400

    Initiating the repo

---
+++ Isotope-enrichment/Scripts/1_Clean-data.R
@@ -0,0 +1,4 @@
+
+d1 <- read.csv("Isotope-enrichment/Raw-data/2020-07-20_n15.csv")
+d2 <- read.csv("Isotope-enrichment/Raw-data/2021-09-20_n15.csv")
+d3 <- read.csv("Isotope-enrichment/Raw-data/2022-02-08_n15.csv")
